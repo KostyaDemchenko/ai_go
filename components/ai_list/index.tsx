76e14f6406881e2a40343018daf0a41cbb884144
KostyaDemchenko <kostya.doge@gmail.com>
2024-03-21 19:06:02 +0200

    added pre loader

--- components/ai_list/index.tsx
+++ components/ai_list/index.tsx
@@ -1,5 +1,3 @@
-// components/ai_list/index.tsx
-
 import React, { useState, useEffect } from "react";
 
 const fetchFromNotion = async (): Promise<aiListStructured[]> => {
@@ -13,7 +11,7 @@ const fetchFromNotion = async (): Promise<aiListStructured[]> => {
 };
 
 const AiList = () => {
-  const [aiList, setAiList] = useState<aiListStructured[]>([]); // Initialize state as an empty array
+  const [aiList, setAiList] = useState<aiListStructured[] | null>(null); // Initialize state as null
 
   useEffect(() => {
     const fetchData = async () => {
@@ -26,6 +24,10 @@ const AiList = () => {
     };
     fetchData();
   }, []); // Empty dependency array to execute the effect only once when the component mounts
+
+  if (aiList === null) {
+    return <div>Loading...</div>; // Render loading indicator while data is being fetched
+  }
 
   return (
     <div>
