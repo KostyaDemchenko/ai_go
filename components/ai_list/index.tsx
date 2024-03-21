36226866dd73c9c6a5e98cbf855c644da592daaa
Jpeg <kostya.doge@gmail.com>
2024-03-21 18:15:22 +0200

    try to fetch

--- components/ai_list/index.tsx
+++ components/ai_list/index.tsx
@@ -1,14 +1,32 @@
-import React from "react";
-
-import "./style.scss";
-
 const fetchFromNotion = async () => {
   const res = await fetch("http://localhost:3000/api/notion_ai_list");
   const data = await res.json();
-  return data;
+  return JSON.parse(data);
 };
 
 export default async function AiList() {
-  const rows: aiListStructured = await fetchFromNotion();
-  return <div></div>;
+  const ai: aiListStructured = await fetchFromNotion();
+  return <code>{ai.ai_name}</code>;
 }
+
+// export async function getServerSideProps() {
+//   try {
+//     const response = await fetch("http://localhost:3000/api/notion_ai_list");
+//     if (!response.ok) {
+//       throw new Error("Failed to fetch data");
+//     }
+//     const data = await response.json();
+//     return {
+//       props: {
+//         aiList: data.aiListStructured
+//       }
+//     };
+//   } catch (error) {
+//     console.error("Error fetching data:", error);
+//     return {
+//       props: {
+//         aiList: []
+//       }
+//     };
+//   }
+// }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
+"use client";
+
 import React from "react";
+import Link from "next/link";
 import "./globals.scss";
 
 export default function Home() {
   return (
     <main>
-      <div></div>
+      <div>
+        <Link href="/AiList">link</Link>
+      </div>
     </main>
   );
 }
