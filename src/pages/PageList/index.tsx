aaee4742a5b7eaaddec294df8ac887fa8264b1ca
KostyaDemchenko <kostya.doge@gmail.com>
2024-03-21 21:48:40 +0200

    updayed page

--- src/pages/PageList/index.tsx
+++ src/pages/PageList/index.tsx
@@ -1,12 +1,11 @@
 "use client";
 
 import React from "react";
-// import AiList from "@/components/ai_list";
 
 export default async function HomePage() {
   return (
     <main>
-      <div>{/* <AiList /> */}</div>
+      <div></div>
     </main>
   );
 }
