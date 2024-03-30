814b761412e6edbfcca9f10b4dad03fa9a1bb1fc
Jpeg <kostya.doge@gmail.com>
2024-03-31 00:50:13 +0200

    Update filter css

--- components/AiFilter/index.tsx
+++ components/AiFilter/index.tsx
@@ -1,5 +1,3 @@
-// components/AiFilter/index.tsx
-
 import React, { useState } from "react";
 import Image from "next/image";
 
@@ -10,27 +8,32 @@ import "./style.scss";
 interface AiFilterProps {
   categories: string[];
   onSelectCategory: (selectedCategories: string[]) => void;
-  filterName: string; // Добавляем prop для передачи заголовка фильтра
+  filterName: string;
 }
 
 const AiFilter: React.FC<AiFilterProps> = ({ categories, onSelectCategory, filterName }) => {
   const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
+  const [isBottomSectionVisible, setIsBottomSectionVisible] = useState<boolean>(false);
 
   const handleCategoryToggle = (category: string) => {
     const updatedCategories = selectedCategories.includes(category)
       ? selectedCategories.filter((cat) => cat !== category)
       : [...selectedCategories, category];
     setSelectedCategories(updatedCategories);
-    onSelectCategory(updatedCategories); // Вызываем функцию onSelectCategory с обновленным списком категорий
+    onSelectCategory(updatedCategories);
+  };
+
+  const toggleBottomSection = () => {
+    setIsBottomSectionVisible(!isBottomSectionVisible);
   };
 
   return (
-    <div className="ai-filter-container">
-      <div className="top-section">
+    <div className={`ai-filter-container ${isBottomSectionVisible ? "open" : ""}`}>
+      <div className="top-section" onClick={toggleBottomSection}>
         <p className="title">{filterName}</p>
         <Image className="icon" src={iconObj.arrowDown} width={16} height={16} alt="Arrow" />
       </div>
-      <div className="bottom-section">
+      <div className={`bottom-section ${isBottomSectionVisible ? "open" : ""}`}>
         {categories.map((category) => (
           <label key={category}>
             <input
