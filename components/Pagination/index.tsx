be196e27bc00fa40944132c749c97ad7ec6562bd
Jpeg <kostya.doge@gmail.com>
2024-03-31 19:24:45 +0300

    Pagination finished

--- components/Pagination/index.tsx
+++ components/Pagination/index.tsx
@@ -1,6 +1,11 @@
 // components/Pagination/index.tsx
 
 import React from "react";
+import Image from "next/image";
+
+import iconObj from "@/public/icons/utils";
+
+import "./style.scss";
 
 interface PaginationProps {
   currentPage: number;
@@ -22,23 +27,42 @@ const Pagination: React.FC<PaginationProps> = ({
 
   return (
     <div className="pagination">
-      <button onClick={() => paginate(currentPage - 1)} disabled={currentPage === 1}>
-        Prev
+      <button
+        onClick={() => paginate(currentPage - 1)}
+        disabled={currentPage === 1}
+        className="prev-page"
+      >
+        <Image
+          className="icon"
+          src={iconObj.paginationBack}
+          width={24}
+          height={24}
+          alt="Previous page"
+        />
       </button>
-      {pageNumbers.map((number) => (
-        <button
-          key={number}
-          onClick={() => paginate(number)}
-          className={currentPage === number ? "active" : ""}
-        >
-          {number}
-        </button>
-      ))}
+      <div className="page-list">
+        {pageNumbers.map((number) => (
+          <button
+            key={number}
+            onClick={() => paginate(number)}
+            className={currentPage === number ? "active" : "inactive"}
+          >
+            {number}
+          </button>
+        ))}
+      </div>
       <button
         onClick={() => paginate(currentPage + 1)}
         disabled={currentPage === Math.ceil(totalItems / itemsPerPage)}
+        className="next-page"
       >
-        Next
+        <Image
+          className="icon"
+          src={iconObj.paginationUp}
+          width={24}
+          height={24}
+          alt="Previous page"
+        />
       </button>
     </div>
   );
--- public/icons/utils.tsx
+++ public/icons/utils.tsx
@@ -1,6 +1,8 @@
 import open_in_new_link from "./open_in_new_link.svg";
-import arrowRight from "./arrow_forward.svg";
 import arrowRightDash from "./arrow_with_dash.svg";
+import paginationBack from "./pagination-back.svg";
+import paginationUp from "./pagination-up.svg";
+import arrowRight from "./arrow_forward.svg";
 import arrowDown from "./arrow_down.svg";
 import search from "./search.svg";
 import check from "./check.svg";
@@ -10,6 +12,8 @@ import mail from "./mail.svg";
 const obj: { [key: string]: string } = {
   open_in_new_link: open_in_new_link,
   arrowRightDash: arrowRightDash,
+  paginationBack: paginationBack,
+  paginationUp: paginationUp,
   arrowRight: arrowRight,
   arrowDown: arrowDown,
   search: search,
