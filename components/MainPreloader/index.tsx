a68a52267c37b267116160c8ed6628d5d8acabb6
KostyaDemchenko <kostya.doge@gmail.com>
2024-04-15 00:08:14 +0300

    Preloader path fix

--- components/MainPreloader/index.tsx
+++ components/MainPreloader/index.tsx
@@ -29,7 +29,7 @@ const MainPreloader: React.FC<MainPreloaderProps> = ({ children }) => {
     <div className="preloader-container">
       {isLoading ? (
         <div className="main-preloader">
-          <img src="/Preloader.gif" alt="Preloader" width={300} height={300} />
+          <img src="/video/preloader.gif" alt="Preloader" width={300} height={300} />
         </div>
       ) : (
         <>{children}</>
