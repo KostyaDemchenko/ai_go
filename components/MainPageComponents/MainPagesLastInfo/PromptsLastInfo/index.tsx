84c31ca35a67d34fcc9cce4d5c3bc9f8b599b58e
KostyaDemchenko <kostya.doge@gmail.com>
2024-05-12 19:56:44 +0300

    Fixed modal for promptLastInfo

--- components/MainPageComponents/MainPagesLastInfo/PromptsLastInfo/index.tsx
+++ components/MainPageComponents/MainPagesLastInfo/PromptsLastInfo/index.tsx
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import { ToastContainer } from "react-toastify";
 import { Swiper, SwiperSlide } from "swiper/react";
+import Rodal from "rodal";
 
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
+import "rodal/lib/rodal.css";
 
 // Preloder
 import MainPagePreloader from "@/components/MainPageComponents/MainPageLastInfoPreloader";
@@ -14,7 +16,6 @@ import MainPagePreloader from "@/components/MainPageComponents/MainPageLastInfoP
 import CartRate from "@/components/PromptsListComponents/CartRating";
 import DownloadBtn from "@/components/BasicСomponents/DownloadBtn";
 import AccordionPromptsItems from "@/components/PromptsListComponents/AccordionPromtsItems";
-import YoutubeModal from "@/components/BasicСomponents/YoutubeModal";
 
 import iconObj from "@/public/icons/utils";
 
@@ -24,6 +25,8 @@ const PromtsLastInfo: React.FC = () => {
   const [promptsList, setPromptsList] = useState<promtsListStructured[] | null>(null);
   const [itemsPerPage] = useState<number>(4);
   const [sortType, setSortType] = useState<string>("newest");
+  const [modalVisible, setModalVisible] = useState<boolean>(false);
+  const [modalVideoUrl, setModalVideoUrl] = useState<string>("");
 
   useEffect(() => {
     fetchData();
@@ -73,6 +76,17 @@ const PromtsLastInfo: React.FC = () => {
     return <MainPagePreloader />;
   }
 
+  const handleOpenModal = (videoUrl: string) => {
+    setModalVideoUrl(videoUrl);
+    setModalVisible(true);
+    document.body.style.overflow = "hidden"; // Prevent scrolling when modal is open
+  };
+
+  const handleCloseModal = () => {
+    setModalVisible(false);
+    document.body.style.overflow = "auto"; // Enable scrolling when modal is closed
+  };
+
   return (
     <>
       <div className="page-last-info-box">
@@ -89,14 +103,24 @@ const PromtsLastInfo: React.FC = () => {
                 <div className="top-box">
                   <CartRate rate={prompt.prompt_rate} />
                   {prompt.promt_result_type.find((type) => type.name === "video") ? (
-                    // Если тип - видео, показываем триггер модального окна
-                    <YoutubeModal
-                      videoTitle={prompt.prompt_name}
-                      videoUrl={prompt.prompt_result_video_url}
-                      imageUrl={prompt.prompt_result_img_url}
-                    />
+                    // If type is video, show modal trigger
+                    <div
+                      className="prev-box"
+                      onClick={() => handleOpenModal(prompt.prompt_result_video_url)}
+                    >
+                      <img
+                        className="prev-img"
+                        src={
+                          "https://i.ytimg.com/vi/" +
+                          prompt.prompt_result_video_url +
+                          "/maxresdefault.jpg"
+                        }
+                        alt="Watch Video"
+                      />
+                      <Image src={iconObj.playBtn} alt="Open modal" className="icon" />
+                    </div>
                   ) : (
-                    // Если тип - изображение, показываем изображение
+                    // If type is image, show image
                     <img
                       className="prev-img"
                       src={prompt.prompt_result_img_url}
@@ -138,55 +162,79 @@ const PromtsLastInfo: React.FC = () => {
           // loop={true}
           className="promptsSwiper"
         >
-          {promptsList.length === 0 ? (
-            <p>No items to display.</p>
-          ) : (
-            promptsList.map((prompt: promtsListStructured, index: number) => (
-              <SwiperSlide key={index} className="prompt-item">
-                <div className="top-box">
-                  <CartRate rate={prompt.prompt_rate} />
-                  {prompt.promt_result_type.find((type) => type.name === "video") ? (
-                    // Если тип - видео, показываем триггер модального окна
-                    <YoutubeModal
-                      videoTitle={prompt.prompt_name}
-                      videoUrl={prompt.prompt_result_video_url}
-                      imageUrl={prompt.prompt_result_img_url}
-                    />
-                  ) : (
-                    // Если тип - изображение, показываем изображение
+          {promptsList.map((prompt: promtsListStructured, index: number) => (
+            <SwiperSlide key={index} className="prompt-item">
+              <div className="top-box">
+                <CartRate rate={prompt.prompt_rate} />
+                {prompt.promt_result_type.find((type) => type.name === "video") ? (
+                  // If type is video, show modal trigger
+                  <div
+                    className="prev-box"
+                    onClick={() => handleOpenModal(prompt.prompt_result_video_url)}
+                  >
                     <img
                       className="prev-img"
-                      src={prompt.prompt_result_img_url}
-                      alt={prompt.prompt_name}
+                      src={
+                        "https://i.ytimg.com/vi/" +
+                        prompt.prompt_result_video_url +
+                        "/maxresdefault.jpg"
+                      }
+                      alt="Watch Video"
                     />
-                  )}
-                  <DownloadBtn downloadLink={prompt.prompt_result_img_url} />
-                </div>
-                <div className="content-box">
-                  <div className="prompt-title-box">
-                    <a className="prompt-ai-title" target="_blank" href={prompt.prompt_ai_url}>
-                      {prompt.prompt_ai_title.map((type: MultiSelectOption) => type.name)}
-                    </a>
-                    <p className="prompt-name">{prompt.prompt_name}</p>
+                    <Image src={iconObj.playBtn} alt="Open modal" className="icon" />
                   </div>
-                  <AccordionPromptsItems promptsContent={prompt.prompt_pattern} />
-                  <div className="property-box">
-                    {[
-                      ...prompt.prompt_type.map((type: MultiSelectOption) => type.name),
-                      ...prompt.prompt_speciality.map((type: MultiSelectOption) => type.name),
-                      ...prompt.prompt_ai_title.map((type: MultiSelectOption) => type.name)
-                    ].map((name: string, index: number) => (
-                      <p className="property" key={index}>
-                        {name}
-                      </p>
-                    ))}
-                  </div>
+                ) : (
+                  // If type is image, show image
+                  <img
+                    className="prev-img"
+                    src={prompt.prompt_result_img_url}
+                    alt={prompt.prompt_name}
+                  />
+                )}
+                <DownloadBtn downloadLink={prompt.prompt_result_img_url} />
+              </div>
+              <div className="content-box">
+                <div className="prompt-title-box">
+                  <a className="prompt-ai-title" target="_blank" href={prompt.prompt_ai_url}>
+                    {prompt.prompt_ai_title.map((type: MultiSelectOption) => type.name)}
+                  </a>
+                  <p className="prompt-name">{prompt.prompt_name}</p>
                 </div>
-              </SwiperSlide>
-            ))
-          )}
+                <AccordionPromptsItems promptsContent={prompt.prompt_pattern} />
+                <div className="property-box">
+                  {[
+                    ...prompt.prompt_type.map((type: MultiSelectOption) => type.name),
+                    ...prompt.prompt_speciality.map((type: MultiSelectOption) => type.name),
+                    ...prompt.prompt_ai_title.map((type: MultiSelectOption) => type.name)
+                  ].map((name: string, index: number) => (
+                    <p className="property" key={index}>
+                      {name}
+                    </p>
+                  ))}
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
         </Swiper>
       </div>
+      <Rodal
+        visible={modalVisible}
+        onClose={handleCloseModal}
+        width={0}
+        height={0}
+        customStyles={{ height: "100%", width: "100%" }}
+        className="youtube-modal"
+      >
+        <iframe
+          width="100%"
+          height="100%"
+          src={"https://www.youtube.com/embed/" + modalVideoUrl}
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          title="Embedded youtube"
+        />
+      </Rodal>
       <ToastContainer />
     </>
   );
